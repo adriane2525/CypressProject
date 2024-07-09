db2879504f18dc2a762be766dceabba07f61b3da
Adriane Torres <adrianetor2525@gmail.com>
2024-07-08 22:51:55 -0400

    First Commit

---
+++ cypress/e2e/CommonFunction.cy.js
@@ -0,0 +1,7 @@
+
+export function LoginFunction(UserName,Password){
+
+    cy.get('input[name="username"]').type(UserName);
+    cy.get('input[name="password"]').type(Password);
+    cy.get('input.button').click();
+} 
